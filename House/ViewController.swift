0684bd925f4b90b4b380f8f77c56787698a553d9
arisupriatna14 <arisupriatna703@gmail.com>
2019-08-25 11:58:56 +0700

    Initial Commit

---
+++ House/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  House
+//
+//  Created by Ari Supriatna on 25/08/19.
+//  Copyright © 2019 Ari Supriatna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
